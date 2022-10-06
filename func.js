2f1365ac0841e2d17d0118cbdb7b63da81c6adfd
vincent lindgren <63017951+vincentlindgren@users.noreply.github.com>
2022-10-06 19:45:58 +0200

    dwa

---
+++ func.js
@@ -0,0 +1,24 @@
+function main(x){
+
+    try{
+        var resourceIdParsedArray = x.split('/');
+        var resourceId = resourceIdParsedArray[4];
+    }
+    catch(error){
+        return error;
+    }
+    return resourceId;
+}
+
+
+// function main(x){
+
+//     try{
+//         var resourceIdParsedArray = x.split('/');
+//         var resourceId = resourceIdParsedArray[4];
+//     }
+//     catch(error){
+//         return error;
+//     }
+//     return resourceId;
+// }
